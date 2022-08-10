a0bb4d54865c46ffcf0233339c7e6bd622ed97b7
TRIDTY <samuelsantanaesposito@gmail.com>
2022-08-10 11:53:06 -0300

    commit inicial SmartDot

---
+++ src/main/java/com/tridev/smartdot/api/SmartDotApplication.java
@@ -0,0 +1,13 @@
+package com.tridev.smartdot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDotApplication.class, args);
+	}
+
+}
